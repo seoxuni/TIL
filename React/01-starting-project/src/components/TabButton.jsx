7c5a1ba715cb13e819eed78a70ce6fa417014902
seoxuni <seoxuni@kakao.com>
2024-07-13 17:13:00 +0900

    update

--- React/01-starting-project/src/components/TabButton.jsx
+++ React/01-starting-project/src/components/TabButton.jsx
@@ -1,11 +1,11 @@
-export default function TabButton(props) {
+export default function TabButton({ children, isSelected, ...props }) {
   return (
     <li>
       <button
-        className={props.isSelected ? "active" : undefined}
-        onClick={props.onSelect}
+        className={isSelected ? "active" : undefined}
+        {...props}
       >
-        {props.children}
+        {children}
       </button>
     </li>
   );
